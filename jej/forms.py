1aad47173ea5b96230830ab82a7056f1fa88f6a1
cooklee <sbbogus@gmail.com>
2021-07-31 16:19:52 +0200

    metoda clean i validacja

--- jej/forms.py
+++ jej/forms.py
@@ -1,11 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from jej.models import Author
 
 
+def check_start_with_s(value):
+    value = value.lower()
+    if not value.startswith('s'):
+        raise ValidationError("Autor musi zaczynać sie na s")
+
+
 class AddAuthorForm(forms.Form):
-    first_name = forms.CharField(max_length=6, error_messages={'required':'Wypełnij to cholero'})
-    last_name = forms.CharField(max_length=10, label='Nazwisko')
+    first_name = forms.CharField(max_length=20, error_messages={'required':'Wypełnij to cholero'})
+    last_name = forms.CharField(max_length=20, label='Nazwisko')
+
+    def clean(self):
+        data = super().clean()
+        if data.get('first_name') == 'slawek' and data.get('last_name') == 'boguslawski':
+            raise ValidationError("no nie tylko nie ten gosciu")
 
 
 class AddBookForm(forms.Form):
